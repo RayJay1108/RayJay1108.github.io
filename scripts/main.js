d6e5ff17379a5479c28e718f5b48b96afb4d8ae9
RayJay1108 <rjvargas0804@gmail.com>
2024-11-17 16:30:12 -0700

    docs: add for loop

--- scripts/main.js
+++ scripts/main.js
@@ -47,4 +47,18 @@ function myFunction() {
   // Console log current date and time // 
   console.log(new Date().toString());   
 
+//for loop lit // 
+
+for (let i = 1; i <= 12; i = i + 1) {
+    const listItem = document.createElement('li');
+
+    if (i % 2 === 0) {
+        listItem.textContent = ('even');
+    }
+
+    else {
+        listItem.textContent = ('odd');
+    }
+    document.getElementById("numbers").appendChild(listItem);
+};
 
