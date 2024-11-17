a0d156aafd988c2c0db4db5f1890e35592983696
RayJay1108 <rjvargas0804@gmail.com>
2024-11-16 19:18:59 -0700

    docs: add js greeting and footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+const time = new Date().getHours();
+let greeting;
+    if (time <= 6) {
+        greeting = "Good Morning";
+    }
+    else if (time <= 12) {
+        greeting = "Good Afternoon";
+    }
+    else if (time <= 18) {
+        greeting = "Good Evening";
+    }
+    document.getElementById ("Greeting").innerHTML = greeting; 
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var currentYear = new Date().getFullYear();
+        document.getElementById('year').textContent = currentYear;
+       }); 
+
+alert ('Hello World!')
